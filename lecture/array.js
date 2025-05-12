808b856c3949c9a307a55e8f944e0d53bf0cc32c
Satgityam <officialimsatyam@gmail.com>
2025-05-12 15:38:19 +0000

    exploring array

---
+++ lecture/array.js
@@ -0,0 +1,26 @@
+let myArray=[1,3,53,6,4,3,5,'satyam','kumar'];
+console.log(myArray);
+console.log(myArray[2]);
+console.log(myArray.push('hewllow'));
+const si=myArray.push('kukum');// gives the updated length of the array
+console.log(si);
+let nember=new Array(1,3,5,5,6,6,6);
+// nember.unshift('satyam'); // push in starting position
+// nember.shift(); // pop from starting positing
+// console.log(nember);
+// console.log(nember.includes('satyam'));
+// console.log(nember.includes(6));
+let newArr=nember.join();
+console.log(newArr);
+console.log(typeof nember.toString());
+console.log(typeof nember);
+console.log(typeof newArr);
+// slice and splice
+let arr=[1,3,,56,66,'thakur','bsdk','tiwari','mc'];
+console.log(typeof arr.splice(2,1,99));
+console.log(arr.toLocaleString());
+console.log(`${arr[7]} bhut bara  ${arr[8]} hai`);
+
+
+
+
