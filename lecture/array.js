386081606418b702544d4f37a1766363796eb0d1
Satgityam <officialimsatyam@gmail.com>
2025-05-13 06:08:33 +0000

    detailed concept of array

--- lecture/array.js
+++ lecture/array.js
@@ -1,9 +1,9 @@
 let myArray=[1,3,53,6,4,3,5,'satyam','kumar'];
-console.log(myArray);
-console.log(myArray[2]);
-console.log(myArray.push('hewllow'));
-const si=myArray.push('kukum');// gives the updated length of the array
-console.log(si);
+// console.log(myArray);
+// console.log(myArray[2]);
+// console.log(myArray.push('hewllow'));
+// const si=myArray.push('kukum');// gives the updated length of the array
+// console.log(si);
 let nember=new Array(1,3,5,5,6,6,6);
 // nember.unshift('satyam'); // push in starting position
 // nember.shift(); // pop from starting positing
@@ -11,16 +11,24 @@ let nember=new Array(1,3,5,5,6,6,6);
 // console.log(nember.includes('satyam'));
 // console.log(nember.includes(6));
 let newArr=nember.join();
-console.log(newArr);
-console.log(typeof nember.toString());
-console.log(typeof nember);
-console.log(typeof newArr);
+// console.log(newArr);
+// console.log(typeof nember.toString());
+// console.log(typeof nember);
+// console.log(typeof newArr);
 // slice and splice
 let arr=[1,3,,56,66,'thakur','bsdk','tiwari','mc'];
-console.log(typeof arr.splice(2,1,99));
-console.log(arr.toLocaleString());
-console.log(`${arr[7]} bhut bara  ${arr[8]} hai`);
+// console.log(typeof arr.splice(2,1,99));
+// console.log(arr.toLocaleString());
+// console.log('satyam',arr.toString());
+// console.log(`${arr[7]} bhut bara  ${arr[8]} hai`);
 
-
+let myHero=['Tony','Hulk','Captian','loki'];
+let myFev=['Wanda','BlackWidow','Falcon','winterSoldier']
+// myHero=myHero.push(myFev);
+// myHero.push(myFev);
+console.log(typeof myHero);
+// console.log(myHero.toString());
+ myHero.concat(myFev);
+console.log(myHero);
 
 
---
+++ lecture/array2.js
@@ -0,0 +1,28 @@
+let heroes_marvel=new Array('Tony','Captain','Hulk','Thor');
+let heroes_myFev=new Array('BlackWidow','Wonda','Strange','Spider');
+// console.log(heroes_marvel);
+// console.log(heroes_myFev);
+// heroes_marvel.push(heroes_myFev);
+// console.log(heroes_myFev.concat(heroes_marvel).toString());
+// console.log('------->>')
+// console.log(heroes_marvel);
+// console.log('------->>');
+// console.log(heroes_myFev);
+// let new_heroes=[...heroes_marvel,...heroes_myFev];// spread operator -> individual ele in one array.
+// console.log(new_heroes);
+// console.log(new_heroes.toString());
+const A_new_Array=[1,3,5,5,[1,3,5],[23,3,5,[3,6,7,7]]];
+// const Floated_Array=A_new_Array.flat(Infinity);// flatten to all depth 
+// console.log(A_new_Array);
+// console.log('-------->>');
+// console.log(Floated_Array.toLocaleString());
+console.log(Array.isArray('kumar'));
+console.log(Array.from('kumkumkumkum'));
+console.log("------->>");
+console.log(Array.from({name:"kumar"}));
+const Array_of=Array.of(7);
+const Array_=new Array(7);
+// console.log(Array_)
+console.log(Array_.length);
+console.log(Array_of);
+
