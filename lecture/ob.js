d7e97b80273b9b5c4c251cf0cb5c8392ee6759b9
Satyam Kumar <officialimsatyam@gmail.com>
2025-07-04 12:12:08 +0530

    objects

---
+++ lecture/ob.js
@@ -0,0 +1,14 @@
+const obj = {
+    name: "satyam",
+    age: 23,
+    address: {
+        city: "motihari",
+        State: "bihar",
+        country: "india"
+    },
+    Education: {
+        School: "JJV",
+        B_Tech: "GKV",
+        M_Tech: "IIITL"
+    }
+}
