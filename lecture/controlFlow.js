ad42ca216b1ec6ce34c3e6c7aac478b9c1ffb1e1
Satyam Kumar <officialimsatyam@gmail.com>
2025-07-09 11:28:55 +0530

    truthy an falsy concept

---
+++ lecture/controlFlow.js
@@ -0,0 +1,81 @@
+// control Flow 
+
+// if (2 == '2') {
+//     console.log("compare without data type")
+// }
+// if (2 === '2') {
+//     console.log("compare with data type")
+// }
+
+
+// let value = 222;
+// if (value == 222) {
+//     const name = "sam"
+//     console.log(`vale is right and name is ${name}`)
+// }
+// console.log(name)
+
+
+// let mony = 1000; 
+// if (mony > 400) console.log("you can buy a gift"),    // not a good practice to write a code in one line using comma
+//     console.log("you can not buy a gift "),
+//     console.log(`you have ${mony} in your wallet `);
+
+
+
+let month = 2
+switch (month) {
+    case 1:
+        console.log("January")
+
+        break;
+    case 2:
+        console.log("February")
+
+        break;
+    case 3:
+        console.log("March")
+
+        break;
+    case 4:
+        console.log("April")
+
+        break;
+    case 5:
+        console.log("May")
+
+        break;
+    case 6:
+        console.log("June")
+
+        break;
+    case 7:
+        console.log("July")
+
+        break;
+    case 8:
+        console.log("August")
+
+        break;
+    case 9:
+        console.log("September")
+
+        break;
+    case 10:
+        console.log("October")
+
+        break;
+    case 11:
+        console.log("November")
+
+        break;
+    case 12:
+        console.log("December")
+
+        break;
+
+
+    default:
+        console.log("Invalid month")
+        break;
+}
---
+++ lecture/truthyFalsy.js
@@ -0,0 +1,2 @@
+// falsy values--> fasle, 0,-0, "", null, undefined, NaN,BigInt(0),
+// NUllish colescing operation (??): null undefined
